9d305efb95c9ee460962e9a15ffbb846fadcf437
PhilipVigus <philipvigus@yahoo.co.uk>
2022-04-17 19:03:37 +0100

    Correctly import English football teams

--- data-imports/src/main/java/com/philvigus/soccerapi/dataimports/DataImportsApplication.java
+++ data-imports/src/main/java/com/philvigus/soccerapi/dataimports/DataImportsApplication.java
@@ -1,21 +1,35 @@
 package com.philvigus.soccerapi.dataimports;
 
-import com.philvigus.soccerapi.domain.services.TeamService;
+import com.philvigus.soccerapi.dataimports.services.TeamImporterService;
+import com.philvigus.soccerapi.dataimports.services.TextFileImporterService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Profile;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+import java.util.List;
 
 @SpringBootApplication
 @ComponentScan(basePackages = {"com.philvigus.soccerapi"})
 @EntityScan("com.philvigus.soccerapi.domain.entities")
 @EnableJpaRepositories(basePackages = {"com.philvigus.soccerapi.domain.repositories"})
-public class DataImportsApplication {
-  @Autowired TeamService teamService;
+@Profile("!test")
+public class DataImportsApplication implements CommandLineRunner {
+  @Autowired TextFileImporterService textFileImporterService;
+  @Autowired TeamImporterService teamImporterService;
 
   public static void main(final String[] args) {
     SpringApplication.run(DataImportsApplication.class, args);
+  }
+
+  @Override
+  public void run(String... args) throws Exception {
+    List<String> data =
+        textFileImporterService.importFile("data/rsss/england/divisional-movements.txt");
+    teamImporterService.importTeams(data);
   }
 }
